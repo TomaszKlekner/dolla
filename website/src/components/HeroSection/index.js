22fce7e1152bb788de4fa2c9b27d176d2d2bd08a
Tomasz Klekner <tomasz.klekner@gmail.com>
2020-12-07 15:56:53 +0100

    Add Hero content and fonts

--- website/src/components/HeroSection/index.js
+++ website/src/components/HeroSection/index.js
@@ -1,13 +1,49 @@
-import React from "react";
-import { HeroContainer, HeroBg, VideoBg } from "./HeroElements";
+import React, { useState } from "react";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+import { Button } from "../ButtonElement";
 import video from "../../videos/video.mp4";
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
   return (
     <HeroContainer id="home">
       <HeroBg>
         <VideoBg autoPlay loop muted src={video} type="video/mp4" />
       </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account toady and recieve $250 in credit towards
+          your next payment.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            primary={true}
+            dark={true}
+            class="primary"
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+          >
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   );
 };
