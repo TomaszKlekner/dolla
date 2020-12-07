33740c5bddae266160ddea70873c22d7d3c852b5
Tomasz Klekner <tomasz.klekner@gmail.com>
2020-12-07 20:03:02 +0100

    Make header transparent on load

--- website/src/components/Navbar/index.js
+++ website/src/components/Navbar/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa";
+import { IconContext } from "react-icons";
 import {
   Nav,
   NavbarContainer,
@@ -13,69 +14,85 @@ import {
 } from "./NavbarElements";
 
 export const NabBar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
+
   return (
     <>
-      <Nav>
-        <NavbarContainer>
-          <NavLogo to="/">dolla</NavLogo>
-          <MobileIcon onClick={toggle}>
-            <FaBars />
-          </MobileIcon>
-          <NavMenu>
-            <NavItem>
-              <NavLink
-                smooth={true}
-                duration={500}
-                spy={true}
-                exact={true}
-                offset={-80}
-                to="about"
-              >
-                About
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                smooth={true}
-                duration={500}
-                spy={true}
-                exact={true}
-                offset={-80}
-                to="discover"
-              >
-                Discover
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                smooth={true}
-                duration={500}
-                spy={true}
-                exact={true}
-                offset={-80}
-                to="services"
-              >
-                Services
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                smooth={true}
-                duration={500}
-                spy={true}
-                exact={true}
-                offset={-80}
-                to="signup"
-              >
-                Sign Up
-              </NavLink>
-            </NavItem>
-          </NavMenu>
-          <NavBtn>
-            <NavBtnLink to="/sign-in">Sign In</NavBtnLink>
-          </NavBtn>
-        </NavbarContainer>
-      </Nav>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav scrollNav={scrollNav}>
+          <NavbarContainer>
+            <NavLogo to="/">dolla</NavLogo>
+            <MobileIcon onClick={toggle}>
+              <FaBars />
+            </MobileIcon>
+            <NavMenu>
+              <NavItem>
+                <NavLink
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact={true}
+                  offset={-80}
+                  to="about"
+                >
+                  About
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact={true}
+                  offset={-80}
+                  to="discover"
+                >
+                  Discover
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact={true}
+                  offset={-80}
+                  to="services"
+                >
+                  Services
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact={true}
+                  offset={-80}
+                  to="signup"
+                >
+                  Sign Up
+                </NavLink>
+              </NavItem>
+            </NavMenu>
+            <NavBtn>
+              <NavBtnLink to="/sign-in">Sign In</NavBtnLink>
+            </NavBtn>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
     </>
   );
 };
